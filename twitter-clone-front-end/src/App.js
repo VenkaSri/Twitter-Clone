7a0945d9dd4bf14310b5a14aee1e5c11a4a9c77d
VenkaSri <venkasritharan@gmail.com>
2023-01-30 20:42:26 -0500

    added google analytics

--- twitter-clone-front-end/src/App.js
+++ twitter-clone-front-end/src/App.js
@@ -4,6 +4,10 @@ import Header from "./components/sidebars/leftsidebar/Header";
 import MainContainer from "./components/main/body/MainContainer";
 import RightSideBar from "./components/sidebars/rightsidebar/RightSideBar";
 import LandingFooter from "./components/footer/LandingFooterModal";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-255822850-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
