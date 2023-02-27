aae87c5f9c02ad48931435f0cd601ecba7393186
VenkaSri <venkasritharan@gmail.com>
2023-02-27 16:10:50 -0500

    validation completed

--- twitter-clone-front-end/src/components/authentication/signup/stepone/NameInputField.js
+++ twitter-clone-front-end/src/components/authentication/signup/stepone/NameInputField.js
@@ -1,42 +1,43 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { TextField } from "@mui/material";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { object, string, number, date, InferType } from 'yup';
 
 const schema = yup.object({
-  name:yup.string().matches(/[^\s\\]/, {message: "Whats your name?"}),
-})
-
-
+  name: yup.string().matches(/[^\s\\]/, { message: "Whats your name?" }),
+});
 
 const NameInputField = () => {
-  const { register, handleSubmit, watch, formState: {errors} } = useForm({
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
-      name: ""
+      name: "",
     },
-    resolver: yupResolver(schema)
+    resolver: yupResolver(schema),
   });
 
-
-  
-
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = (data) => console.log(data);
   useEffect(() => {
     const subscription = watch(handleSubmit(onSubmit));
     return () => subscription.unsubscribe();
-}, [handleSubmit, watch]);
+  }, [handleSubmit, watch]);
 
-const nameInputClassess = errors.name 
-? "border border-[#ff0000] h-[3.688rem] group rounded-[4px] focus-within:border-2 focus-within:border-[#ff0000] !bg-[#fff] max-h-[3.688rem]"
-: "border border-[#CFD9DE] h-[3.688rem] group rounded-[4px] focus-within:border-2 focus-within:border-[#1d9bf0] !bg-[#fff] max-h-[3.688rem]"; 
-  
+  const nameInputClassess = errors.name
+    ? "border border-[#ff0000] h-[3.688rem] group rounded-[4px] focus-within:border-2 focus-within:border-[#ff0000] !bg-[#fff] max-h-[3.688rem]"
+    : "border border-[#CFD9DE] h-[3.688rem] group rounded-[4px] focus-within:border-2 focus-within:border-[#1d9bf0] !bg-[#fff] max-h-[3.688rem]";
+
+  const nameInputLabelClasses = errors.name ? "#ff0000" : "#1d9bf0";
+
   return (
     <div className="flex flex-col grow">
       <TextField
-      {...register("name")}
+        {...register("name")}
         name="name"
         type="text"
         id="outlined-basic"
@@ -46,9 +47,17 @@ const nameInputClassess = errors.name
           className: nameInputClassess,
           disableUnderline: true,
         }}
-  
+        sx={{
+          "& label": {
+            "&.Mui-focused": {
+              color: nameInputLabelClasses,
+            },
+          },
+        }}
       />
-      <p>{errors.name?.message}</p>
+      <p className="font-cReg text-[14px] ml-2 text-[#ff0000]">
+        {errors.name?.message}
+      </p>
     </div>
   );
 };
