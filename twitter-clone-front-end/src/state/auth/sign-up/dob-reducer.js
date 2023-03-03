9a409e8d730672b9b7a21d747930b51e0c123ba8
VenkaSri <venkasritharan@gmail.com>
2023-03-03 15:41:32 -0500

    added a slice function for dob

---
+++ twitter-clone-front-end/src/state/auth/sign-up/dob-reducer.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const dobInitialState = {
+  month: "",
+  day: "",
+  year: "",
+  isDOBSet: false
+}
+
+const dobSlice = createSlice({
+  name: "dob",
+  initialState: dobInitialState,
+  reducers: {
+    setMonth(state, action) {
+      state.month = action.payload;
+    },
+    setDay(state, action) {
+      state.day = action.payload;
+    },
+    setYear(state, action) {
+      state.year = action.payload;
+    }
+  }
+});
+
+export const dobActions = dobSlice.actions;
+export default dobSlice.reducer;
--- twitter-clone-front-end/src/state/index.js
+++ twitter-clone-front-end/src/state/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import nameReducer from "../state/auth/sign-up/StepOne";
+import emailReducer from "./auth/sign-up/email-reducer";
+import dobReducer from "./auth/sign-up/dob-reducer";
 
 
 const store = configureStore({
-  reducer: { name: nameReducer},
+  reducer: { name: nameReducer, email:emailReducer, dob:dobReducer},
 });
 
 export default store;
