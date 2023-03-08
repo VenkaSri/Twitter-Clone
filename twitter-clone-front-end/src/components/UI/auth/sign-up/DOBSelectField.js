0521af44a89a7017110bd929def7c4a70b2c0a96
VenkaSri <venkasritharan@gmail.com>
2023-03-08 18:04:02 -0500

    added the logic to save selected value to state

--- twitter-clone-front-end/src/components/UI/auth/sign-up/DOBSelectField.js
+++ twitter-clone-front-end/src/components/UI/auth/sign-up/DOBSelectField.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import NativeSelect from "@mui/material/NativeSelect";
 import { FormControl, InputLabel } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useDispatch, useSelector } from "react-redux";
+import { dobActions } from "../../../../state/auth/sign-up/dob-reducer";
+import { stepOneActions } from "../../../../state/auth/sign-up/stepone-reducer";
 
 const DOBSelectField = (props) => {
+  const dispatch = useDispatch();
+  const dob = useSelector((state) => state.dob);
+  const selectedValueHandler = (event) => {
+    switch (props.label) {
+      case "Month":
+        dispatch(dobActions.setMonth(event.target.value));
+        break;
+      case "Day":
+        dispatch(dobActions.setDay(event.target.value));
+        break;
+      case "Year":
+        dispatch(dobActions.setYear(event.target.value));
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    if (dob.month !== "" && dob.day !== "" && dob.year !== "") {
+      dispatch(stepOneActions.setDOBEntered(true));
+    }
+  }, [dob.month, dob.year, dob.day, dispatch]);
 
   return (
     <FormControl
@@ -33,7 +59,7 @@ const DOBSelectField = (props) => {
           className: " !ml-[6px] !mt-[6px] !text-[18px]",
         }}
         IconComponent={KeyboardArrowDownIcon}
-        onChange={props.onChange}
+        onChange={selectedValueHandler}
       >
         <option disabled value={30}></option>
         {props.field}
