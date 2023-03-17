e47401c077fa52ce49c1a5f59b504ee1cbe830a3
VenkaSri <venkasritharan@gmail.com>
2023-03-17 17:16:38 -0400

    updated component to make it responsive by adding MUI DialogTItle, content, actions

--- twitter-clone-front-end/src/components/UI/home/FormDialog.js
+++ twitter-clone-front-end/src/components/UI/home/FormDialog.js
@@ -2,9 +2,6 @@ import React from "react";
 
 import Dialog from "@mui/material/Dialog";
 import { DialogActions, DialogContent, DialogTitle } from "@mui/material";
-import StepsHeader from "../../authentication/signup/StepsHeader";
-import StepOne from "../../authentication/signup/stepone/StepOne";
-import StepsFooter from "../../authentication/signup/StepsFooter";
 
 const FormDialog = (props) => {
   return (
@@ -18,11 +15,13 @@ const FormDialog = (props) => {
             borderRadius: "20px",
             display: "flex",
           },
-        }} 
+        }}
       >
-        <DialogTitle style={{padding: 0}}><StepsHeader /></DialogTitle>
-        <DialogContent style={{padding: 0}}><StepOne /></DialogContent>
-        <DialogActions style={{padding: 0, display: "flex"}}><StepsFooter /></DialogActions>
+        <DialogTitle style={{ padding: 0 }}>{props.header}</DialogTitle>
+        <DialogContent style={{ padding: 0 }}>{props.steps}</DialogContent>
+        <DialogActions style={{ padding: 0, display: "flex" }}>
+          {props.footer}
+        </DialogActions>
       </Dialog>
     </div>
   );
