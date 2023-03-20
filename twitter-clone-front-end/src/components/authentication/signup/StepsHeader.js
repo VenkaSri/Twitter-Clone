02571ae1d7f5afc5a3bf273043bd8e6e867a05c7
VenkaSri <venkasritharan@gmail.com>
2023-03-20 14:19:52 -0400

    updated the steps header link based on the step number

--- twitter-clone-front-end/src/components/authentication/signup/StepsHeader.js
+++ twitter-clone-front-end/src/components/authentication/signup/StepsHeader.js
@@ -19,16 +19,18 @@ const StepsHeader = () => {
   };
   return (
     <div className="flex items-center sticky top-0 bg-[#fff] z-50 h-[3.313rem] w-[600px] max-w-[600px]">
-      <Link
-        role="button"
-        to={currentStep > 1 ? "/i/flow/signup" : "/"}
-        onClick={currentStep > 1 ? handleBack : handleReset}
-        className="ml-4 hover:bg-[#E6E7E7] w-[2.125rem] h-[2.125rem] flex items-center justify-center rounded-full cursor-pointer"
-      >
-        <div className="w-[1.25rem] h-[1.25rem]">
-          <SVG svgPath={currentStep > 1 ? FORM_BACK_BUTTON : CLOSE} />
-        </div>
-      </Link>
+      {(currentStep === 1 || currentStep === 2) && (
+        <Link
+          role="button"
+          to={currentStep > 1 ? "/i/flow/signup" : "/"}
+          onClick={currentStep > 1 ? handleBack : handleReset}
+          className="ml-4 hover:bg-[#E6E7E7] w-[2.125rem] h-[2.125rem] flex items-center justify-center rounded-full cursor-pointer"
+        >
+          <div className="w-[1.25rem] h-[1.25rem]">
+            <SVG svgPath={currentStep > 1 ? FORM_BACK_BUTTON : CLOSE} />
+          </div>
+        </Link>
+      )}
       <span className="ml-6 text-xl font-cBold">Step {currentStep} of 3</span>
     </div>
   );
