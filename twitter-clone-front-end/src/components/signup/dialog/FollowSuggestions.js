ce779c29bf05bacb2a917ff9469c35b608573bee
VenkaSri <venkasritharan@gmail.com>
2023-04-10 22:11:23 -0400

    added Skeleton from MUI to display a placerholder preview of all the accounts before they load

--- twitter-clone-front-end/src/components/signup/dialog/FollowSuggestions.js
+++ twitter-clone-front-end/src/components/signup/dialog/FollowSuggestions.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 
 import FollowCard from "../../FollowCard";
 import axios from "axios";
+import { Skeleton } from "@mui/material";
 
 const FollowSuggestions = () => {
-
+  const [loading, setLoading] = useState(false);
   const [followCards, setFollowCards] = useState([]);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get(process.env.REACT_APP_GET_ALL_ACCOUNTS)
       .then((response) => {
@@ -16,6 +18,7 @@ const FollowSuggestions = () => {
           <FollowCard key={user.username} user={user} />
         ));
         setFollowCards(followCards);
+        setLoading(false);
       })
       .catch((error) => console.error(error));
   }, []);
@@ -24,7 +27,8 @@ const FollowSuggestions = () => {
   
   return (
     <>
-      {followCards}
+    {loading ? <Skeleton variant="rectangular" width={425} height={72} /> : followCards}
+      {/* {followCards} */}
     </>
   );
 };
