31f565d418be8090385dbd8b621d3b70aa18a2f0
VenkaSri <venkasritharan@gmail.com>
2023-04-10 20:38:10 -0400

    updated the final step dialog footer

--- twitter-clone-front-end/src/components/signup/dialog/FinaStepFooter.js
+++ twitter-clone-front-end/src/components/signup/dialog/FinaStepFooter.js
@@ -4,7 +4,7 @@ import React from "react";
 const FinalStepFooter = () => {
   return (
     <div className="w-full h-[6.25rem] max:h-[6.25rem] flex justify-center items-center">
-      <button>Hello</button>
+      <button className="w-[440px] h-[52px] font-cBold text-[#fff] rounded-full bg-[#000] hover:bg-[#272c30]">Next</button>
     </div>
   );
 };
