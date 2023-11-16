9203983a263e5d0722e05e3761baf6b7d921b968
VenkaSri <venkasritharan@gmail.com>
2023-11-15 19:15:35 -0500

    Untrack jenkins/ directory

--- jenkins/pipeline/java-app/src/main/java/ca/venkasritharan/twitterclone/repository/user/ProfileCountRepository.java
+++
@@ -1,7 +0,0 @@
-package ca.venkasritharan.twitterclone.repository.user;
-
-import ca.venkasritharan.twitterclone.entity.user.ProfileCount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileCountRepository extends JpaRepository<ProfileCount, Long> {
-}
--- jenkins/pipeline/java-app/src/main/java/ca/venkasritharan/twitterclone/service/UsernameService.java
+++
@@ -1,9 +0,0 @@
-package ca.venkasritharan.twitterclone.service;
-
-import ca.venkasritharan.twitterclone.response.Response;
-import org.springframework.http.ResponseEntity;
-
-public interface UsernameService {
-
-  ResponseEntity<Response<String>> checkIfUsernameIsAvailable(String username);
-}
