392fc2e5f260af7ab0e2c545daf2323c61e199bd
VenkaSri <venkasritharan@gmail.com>
2023-11-18 14:39:29 -0500

    fixed jenkins workspace paths

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "* Testing *"
+WORKSPACE=/var/lib/jenkins/workspace/pipeline-backend
 
-
-docker run --rm -v "$PWD/java-app":/app -v "/Users/venkateshsritharan/.m2":/root/.m2 -w /app maven:3.9.4-amazoncorretto-17 "$@"
+docker run --rm -v "$WORKSPACE/java-app":/app -v "/Users/venkateshsritharan/.m2":/root/.m2 -w /app maven:3.9.4-amazoncorretto-17 "$@"
 
 
