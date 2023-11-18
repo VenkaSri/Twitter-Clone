45b9fa2cea07801f7df888f3900799de471bcce5
VenkaSri <venkasritharan@gmail.com>
2023-11-18 14:53:37 -0500

    fixed path in test stage

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -3,6 +3,6 @@
 echo "* Testing *"
 WORKSPACE=/var/lib/jenkins/workspace/pipeline-backend
 
-docker run --rm -v "$WORKSPACE/java-app":/app -v "/Users/venkateshsritharan/.m2":/root/.m2 -w /app maven:3.9.4-amazoncorretto-17 "$@"
+docker run --rm -v "$WORKSPACE/twitter-clone-back-end":/app -v "/Users/venkateshsritharan/.m2":/root/.m2 -w /app maven:3.9.4-amazoncorretto-17 "$@"
 
 
