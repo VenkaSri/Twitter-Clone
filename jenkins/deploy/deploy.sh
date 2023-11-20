a1b21766a05211a4792ab88fae25195f8e579d03
VenkaSri <venkasritharan@gmail.com>
2023-11-20 14:57:06 -0500

    updated ec2 user

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth 
 
 
-scp -i /opt/prod /tmp/.auth prod-user@ec2-3-133-55-26.us-east-2.compute.amazonaws.com:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@ec2-3-133-55-26.us-east-2.compute.amazonaws.com:/tmp/publish
-ssh -i /opt/prod prod-user@ec2-3-133-55-26.us-east-2.compute.amazonaws.com "~/maven/publish.sh"
+scp -i /opt/prod /tmp/.auth prod@ec2-3-133-55-26.us-east-2.compute.amazonaws.com:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish prod@ec2-3-133-55-26.us-east-2.compute.amazonaws.com:/tmp/publish
+ssh -i /opt/prod prod@ec2-3-133-55-26.us-east-2.compute.amazonaws.com "~/maven/publish.sh"
