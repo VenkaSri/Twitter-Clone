fcd6a2a1ab7161dd305ea46c30308a36311f1cfa
VenkaSri <venkasritharan@gmail.com>
2023-11-25 14:59:18 -0500

    cahnged build to arm image

--- jenkins/build-react/build.sh
+++ jenkins/build-react/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 WORKSPACE=/var/lib/jenkins/workspace/pipeline-backend
 
-docker build -t test-frontend:$BUILD_TAG -f jenkins/build-react/Dockerfile .
+docker buildx create --name mybuilder --use
+docker buildx inspect --bootstrap
+
+
+
+docker buildx build --platform linux/arm64 -t test-frontend:$BUILD_TAG -f jenkins/build-react/Dockerfile --push .
 
 
