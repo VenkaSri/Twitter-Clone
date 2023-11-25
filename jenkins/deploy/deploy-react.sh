d28abc839d86e7c24c0f14084ce640c752c70a0f
VenkaSri <venkasritharan@gmail.com>
2023-11-25 14:13:34 -0500

    updated deploy script

--- jenkins/deploy/deploy-react.sh
+++ jenkins/deploy/deploy-react.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-WORKSPACE=/var/lib/jenkins/workspace/pipeline-backend
-aws s3 cp $WORKSPACE/twitter-clone-front-end/dist/index.html s3://tc-frontend-test/index.html
-aws s3 cp $WORKSPACE/twitter-clone-front-end/dist/vite.svg s3://tc-frontend-test/vite.svg
-aws s3 cp $WORKSPACE/twitter-clone-front-end/dist/assets/ s3://tc-frontend-test/assets/ --recursive
+
+
+echo test-frontend > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+echo $PASS >> /tmp/.auth
+
+
+ssh -i /opt/prod prod@ec2-18-117-92-110.us-east-2.compute.amazonaws.com "~/app/publish.sh"
