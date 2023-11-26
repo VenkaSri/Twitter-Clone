2a0a89b52f1aa6da71680fc1f18964fd4e352189
VenkaSri <venkasritharan@gmail.com>
2023-11-26 11:14:37 -0500

    updated ec2 instance

--- jenkins/deploy/deploy-react.sh
+++ jenkins/deploy/deploy-react.sh
@@ -5,6 +5,6 @@ echo test-frontend > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod@ec2-18-117-92-110.us-east-2.compute.amazonaws.com:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod@ec2-18-117-92-110.us-east-2.compute.amazonaws.com:/tmp/publish
-ssh -i /opt/prod prod@ec2-18-117-92-110.us-east-2.compute.amazonaws.com "~/app/publish.sh"
+scp -i /opt/prod /tmp/.auth prod@ec2-18-222-5-209.us-east-2.compute.amazonaws.com:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish prod@ec2-18-222-5-209.us-east-2.compute.amazonaws.com:/tmp/publish
+ssh -i /opt/prod prod@ec2-18-222-5-209.us-east-2.compute.amazonaws.com "~/app/publish.sh"
