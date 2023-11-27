ae4ccbcf606826bad78580b427c855c7adaa1d6e
VenkaSri <venkasritharan@gmail.com>
2023-11-26 19:20:00 -0500

    removed env

--- jenkins/build-react/build.sh
+++ jenkins/build-react/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-
-docker build -t test-frontend:$BUILD_TAG -f jenkins/build-react/Dockerfile .
+docker build -t test-frontend:$BUILD_TAG -f jenkins/build-react/Dockerfile \
+--build-arg VITE_AUTH_BASE_URL=$VITE_AUTH_BASE_URL \
+--build-arg VITE_POST_BASE_URL=$VITE_POST_BASE_URL \
+--build-arg VITE_USER_BASE_URL=$VITE_USER_BASE_URL .
