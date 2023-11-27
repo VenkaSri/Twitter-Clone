84b5fd5d93b1efce60a5394838f3cbcc107ca091
VenkaSri <venkasritharan@gmail.com>
2023-11-26 19:54:53 -0500

    debugging statements

--- jenkins/build-react/build.sh
+++ jenkins/build-react/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+  
+
+
+echo "VITE_AUTH_BASE_URL: $VITE_AUTH_BASE_URL"
+echo "VITE_POST_BASE_URL: $VITE_POST_BASE_URL"
+echo "VITE_USER_BASE_URL: $VITE_USER_BASE_URL"
+
+
 
 docker build -t test-frontend:$BUILD_TAG -f jenkins/build-react/Dockerfile \
 --build-arg VITE_AUTH_BASE_URL=$VITE_AUTH_BASE_URL \
