80efb135a77c5c6cb1d4b519fa8aafe71e669f12
VenkaSri <venkasritharan@gmail.com>
2023-11-29 11:52:03 -0500

    changed ec2 instances

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth 
 
 
-scp -i /opt/prod /tmp/.auth prod@ec2-3-22-58-36.us-east-2.compute.amazonaws.com:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod@ec2-3-22-58-36.us-east-2.compute.amazonaws.com:/tmp/publish
-ssh -i /opt/prod prod@ec2-3-22-58-36.us-east-2.compute.amazonaws.com "~/maven/publish.sh"
+scp -i /opt/prod /tmp/.auth prod@ec2-34-230-90-152.compute-1.amazonaws.com:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish prod@ec2-34-230-90-152.compute-1.amazonaws.com:/tmp/publish
+ssh -i /opt/prod prod@ec2-34-230-90-152.compute-1.amazonaws.com "~/maven/publish.sh"
