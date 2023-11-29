8d2fed9e041db7087398f81e7e6bc11b3bec766e
VenkaSri <venkasritharan@gmail.com>
2023-11-29 17:50:31 -0500

    hid the ec2 instance ip

--- jenkins/deploy/deploy-react.sh
+++ jenkins/deploy/deploy-react.sh
@@ -5,6 +5,6 @@ echo test-frontend > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod@ec2-54-167-18-30.compute-1.amazonaws.com:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod@ec2-54-167-18-30.compute-1.amazonaws.com:/tmp/publish
-ssh -i /opt/prod prod@ec2-54-167-18-30.compute-1.amazonaws.com "~/app/publish.sh"
+scp -i /opt/prod /tmp/.auth prod@$REACT_SERVER_IP:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish prod@$REACT_SERVER_IP:/tmp/publish
+ssh -i /opt/prod prod@$REACT_SERVER_IP "~/app/publish.sh"
--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth 
 
 
-scp -i /opt/prod /tmp/.auth prod@ec2-34-230-90-152.compute-1.amazonaws.com:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod@ec2-34-230-90-152.compute-1.amazonaws.com:/tmp/publish
-ssh -i /opt/prod prod@ec2-34-230-90-152.compute-1.amazonaws.com "~/maven/publish.sh"
+scp -i /opt/prod /tmp/.auth prod@$SPRING_SERVER_IP:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish prod@$SPRING_SERVER_IP:/tmp/publish
+ssh -i /opt/prod prod@$SPRING_SERVER_IP "~/maven/publish.sh"
