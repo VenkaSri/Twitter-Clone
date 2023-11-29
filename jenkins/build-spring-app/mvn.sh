1df90e5fae29f163dc4a63f33c6c1bd990fcd1ee
VenkaSri <venkasritharan@gmail.com>
2023-11-29 12:33:43 -0500

    updated path

--- jenkins/build-spring-app/mvn.sh
+++ jenkins/build-spring-app/mvn.sh
@@ -3,8 +3,8 @@
 echo "***********************************"
 echo "******** Building Jar *************"
 echo "***********************************"
-WORKSPACE=/var/lib/jenkins/workspace/pipeline-backend
+WORKSPACE=/var/lib/jenkins/workspace/twitterclone
 
-docker run --rm -v "$WORKSPACE/twitter-clone-back-end":/app -v "/Users/venkateshsritharan/.m2":/root/.m2 -w /app maven:3.9.4-amazoncorretto-17 "$@"
+docker run --rm -v "$WORKSPACE/twitter-clone-back-end":/app -v "/var/lib/jenkins/.m2":/root/.m2 -w /app maven:3.9.4-amazoncorretto-17 "$@"
 
 
