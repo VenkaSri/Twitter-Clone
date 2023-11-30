251e5a1f876a32c00fbcd88c51fc98b017bacb0b
VenkaSri <venkasritharan@gmail.com>
2023-11-30 09:44:41 -0500

    delete duplicates

--- twitter-clone-back-end/src/main/java/ca/venkasritharan/twitterclone/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package ca.venkasritharan.twitterclone.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-  public UserAlreadyExistsException(String message) {
-    super(message);
-  }
-}
