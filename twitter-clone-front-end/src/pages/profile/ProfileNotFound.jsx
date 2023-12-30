cb643a5dfb6e590df6c5db1e4f826b41daa93ad8
VenkaSri <venkasritharan@gmail.com>
2023-12-29 20:38:44 -0500

    profile page not found component complete

--- twitter-clone-front-end/src/pages/profile/ProfileNotFound.jsx
+++ twitter-clone-front-end/src/pages/profile/ProfileNotFound.jsx
@@ -1,9 +1,9 @@
-import ProfilePicture from "@/components/ProfilePicture";
 import Head from "@/components/head/Head";
 import { MainSectionHeader } from "@/components/layout/MainSectionHeader";
-import React from "react";
+import { useParams } from "react-router-dom";
 
 export const ProfileNotFound = () => {
+  const { username } = useParams();
   return (
     <>
       <Head title="Profile" />
@@ -17,14 +17,14 @@ export const ProfileNotFound = () => {
           <div className="flex relative items-start justify-between">
             <div className="w-[145.5px] h-[145.5px] -mt-[15%]  mb-3 overflow-visible  rounded-full  dark:bg-black bg-white relative flex justify-center items-center">
               <div className="">
-                <ProfilePicture size={141.5} />
+                <div className="h-[137.5px] w-[137.5px] bg-[#16181C] rounded-full"></div>
               </div>
             </div>
           </div>
           <div className="mb-3 mt-1">
             <div className="flex flex-col grow overflow-hidden">
               <div className="text-black dark:text-white break-words whitespace-nowrap text-ellipsis overflow-hidden font-cBold leading-6 text-[20px]">
-                <span>@fadf</span>
+                <span>@{username}</span>
               </div>
             </div>
           </div>
@@ -33,9 +33,13 @@ export const ProfileNotFound = () => {
           </div>
         </div>
       </div>
-      <div className="br my-8 px-5 mx-auto">
-        <span className="text-[31px] font-cBold">
-          This account doesn't exist
+      <div className="my-8 mx-auto px-5 flex flex-col">
+        <div className="text-[31px] font-cBold leading-9 min-w-0 break-words mb-2 text-left max-w-[350px]">
+          <span>This account doesn&apos;t exist</span>
+        </div>
+
+        <span className="text-[15px] leading-5 font-cR text-[#71767B]">
+          Try searching for another
         </span>
       </div>
     </>
